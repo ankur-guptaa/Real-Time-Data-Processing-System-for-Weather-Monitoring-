442c661ca808a36bedc9bdcc870caa5f1418fb6f
Ankur Gupta <78300998+ankur-27@users.noreply.github.com>
2024-10-25 20:27:25 +0530

    Trick

--- server/index.js
+++ server/index.js
@@ -16,8 +16,13 @@ cron.schedule("0,5,10,15,20,25,30,35,40,45,50,55 * * * *", openWeatherAPI, { sch
 cron.schedule("30 56 23 * * *", recordDailySummary, { scheduled: true, timezone: "Asia/Kolkata" });
 // // openWeatherAPI();
 // recordDailySummary();
+cron.schedule("3,13,23,33,43,53 * * * *", async () => {
+    const act = await axios.get("https://rule-engine-with-ast-zgt4.onrender.com/active");
+    console.log(act.data);
+}, { scheduled: true, timezone: "Asia/Kolkata" });
 
 app.post("/add_city", addCity);
+app.get("/active", (req, res) => { res.send("Acitvated") });
 
 
 app.listen(process.env.PORT, () => {
